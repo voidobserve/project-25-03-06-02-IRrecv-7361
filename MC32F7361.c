43c9b40fe49048d03dfd1ed56a69fd78be1f7d63
voidobserve <99012263+voidobserve@users.noreply.github.com>
2025-03-12 18:03:33 +0800

    2025-03-12-周三，存档

---
+++ MC32F7361.c
@@ -0,0 +1,475 @@
+/******************************************************************************
+;  *       @型号                 : MC32F7361
+;  *       @创建日期             : 2021.12.21
+;  *       @公司/作者            : SINOMCU-FAE
+;  *       @晟矽微技术支持       : 2048615934
+;  *       @晟矽微官网           : http://www.sinomcu.com/
+;  *       @版权                 : 2021 SINOMCU公司版权所有.
+;  *----------------------摘要描述---------------------------------
+;  *                定时器
+;  *                T0   P10 2mS翻转
+;  *                T1   P11 1mS翻转
+;  *                T2   P12 100uS翻转
+;  *                T3   P14 50uS翻转
+******************************************************************************/
+
+#include "user.h"
+
+// 100us延时，前提条件 ： FCPU = FHOSC / 4
+void delay_100us(void)
+{
+    u8 i = 74;
+    while (i--)
+    {
+        Nop();
+    }
+}
+
+// 200us延时，前提条件 ： FCPU = FHOSC / 4
+void delay_200us(void)
+{
+    u8 i = 150;
+    while (i--)
+    {
+        Nop();
+    }
+}
+
+// 1.4ms延时
+void delay_1_4ms(void)
+{
+    u8 i;
+    for (i = 0; i < 14; i++)
+    {
+        delay_100us();
+    }
+}
+
+// 控制信号是以 若干低电平脉冲 + 28ms高电平 + 若干低电平脉冲
+void send_cmd(const u8 data)
+{
+    u8 i;
+
+    if (0 == flag_is_dev_open)
+    {
+        return;
+    }
+
+    for (i = 0; i < 8; i++)
+    {
+        LED_CTL_PIN = 1;
+        delay_100us();
+        LED_CTL_PIN = 0;
+        if ((data >> (7 - i)) & 0x01) // 如果是逻辑1
+        {
+
+            delay_ms(3);
+        }
+        else // 如果是逻辑0
+        {
+            delay_1_4ms();
+        }
+    }
+
+    LED_CTL_PIN = 1;
+    delay_100us();
+    LED_CTL_PIN = 0;
+    delay_ms(33);
+}
+
+/************************************************
+;  *    @函数名          : CLR_RAM
+;  *    @说明            : 清RAM
+;  *    @输入参数        :
+;  *    @返回参数        :
+;  ***********************************************/
+void CLR_RAM(void)
+{
+    for (FSR0 = 0; FSR0 < 0xff; FSR0++)
+    {
+        INDF0 = 0x00;
+    }
+    FSR0 = 0xFF;
+    INDF0 = 0x00;
+}
+/************************************************
+;  *    @函数名            : IO_Init
+;  *    @说明              : IO初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void IO_Init(void)
+{
+    IOP0 = 0x00;   // io口数据位
+    OEP0 = 0x3F;   // io口方向 1:out  0:in
+    PUP0 = 0x00;   // io口上拉电阻   1:enable  0:disable
+    PDP0 = 0x00;   // io口下拉电阻   1:enable  0:disable
+    P0ADCR = 0x00; // io类型选择  1:模拟输入  0:通用io
+
+    IOP1 = 0x00;   // io口数据位
+    OEP1 = 0xFF;   // io口方向 1:out  0:in
+    PUP1 = 0x00;   // io口上拉电阻   1:enable  0:disable
+    PDP1 = 0x00;   // io口下拉电阻   1:enable  0:disable
+    P1ADCR = 0x00; // io类型选择  1:模拟输入  0:通用io
+
+    IOP2 = 0x00; // io口数据位
+    OEP2 = 0x0F; // io口方向 1:out  0:in
+    PUP2 = 0x00; // io口上拉电阻   1:enable  0:disable
+    PDP2 = 0x00; // io口下拉电阻   1:enable  0:disablea
+
+    PMOD = 0x00;  // P00、P01、P13 io端口值从寄存器读，推挽输出
+    DRVCR = 0x80; // 普通驱动
+}
+
+/************************************************
+;  *    @函数名            : TIMER2_INT_Init
+;  *    @说明              : 定时器2初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void TIMER2_INT_Init(void)
+{
+    T2CR = DEF_SET_BIT0 | DEF_SET_BIT1; // CPU,8分频
+    T2CNT = 100 - 1;
+    T2LOAD = 100 - 1; // 100us
+    T2EN = 1;
+    T2IE = 1;
+}
+/************************************************
+;  *    @函数名            : Sys_Init
+;  *    @说明              : 系统初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void Sys_Init(void)
+{
+    GIE = 0;
+    CLR_RAM();
+    IO_Init();
+    // TIMER0_INT_Init();
+    // TIMER1_INT_Init();
+    TIMER2_INT_Init();
+    // TIMER3_INT_Init();
+    GIE = 1;
+}
+
+void led_mode_handle(u8 mode)
+{
+    u8 tmp = cur_led_status;
+    // cur_led_status = rf_data;
+
+    if (mode == 0x01)
+    {
+        LED_CTL_PIN = 1;
+        delay_ms(5);
+        send_cmd(1);
+        flag_is_dev_open = 1; // 打开灯光
+    }
+    else if (mode == 0x03)
+    {
+        LED_CTL_PIN = 0;
+        flag_is_dev_open = 0; // 关闭灯光
+    }
+    else if (mode == 0x04)
+    {
+        send_cmd(1);
+    }
+    else if (mode == 0x06)
+    {
+        send_cmd(10);
+    }
+    else if (mode == 0x07)
+    {
+        send_cmd(2);
+    }
+    else if (mode == 0x09)
+    {
+        send_cmd(9);
+    }
+    else if (mode == 0x0A)
+    {
+        send_cmd(3);
+    }
+    else if (mode == 0x0C)
+    {
+        send_cmd(8);
+    }
+    else if (mode == 0x0D)
+    {
+        send_cmd(4);
+    }
+    else if (mode == 0x0F)
+    {
+        send_cmd(7);
+    }
+    else if (mode == 0x10)
+    {
+        send_cmd(5);
+    }
+    else if (mode == 0x12)
+    {
+        send_cmd(6);
+    }
+    else
+    {
+        // 如果是无效的键值
+        cur_led_status = tmp; // 让灯光的状态不变
+    }
+}
+
+/************************************************
+;  *    @函数名            : main
+;  *    @说明              : 主程序
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void main(void)
+{
+    Sys_Init();
+
+// 红外信号接收引脚：
+#if USE_MY_DEBUG
+
+    P21PU = 1;
+    P21OE = 0;
+
+#else
+
+    P15PU = 1; // 上拉
+    P15OE = 0; // 输入模式
+
+#endif
+
+    // 按键检测引脚：
+
+    flag_is_dev_open = 1;
+
+    while (1)
+    {
+        // DEBUG_PIN = ~DEBUG_PIN;
+        // // delay_200us();
+        // delay_1_4ms();
+
+        // send_cmd(0x96);
+        // send_cmd(0x45);
+
+        // 是按键按下时就做对应的操作，还是按键松开时做对应操作
+
+        if (flag_is_recved_data)
+        {
+            if (ir_data == 0x08)
+            {
+                LED_CTL_PIN = 0;
+                delay_ms(2);
+                LED_CTL_PIN = 1;
+                delay_100us();
+                delay_100us();
+                LED_CTL_PIN = 0;
+                delay_ms(3);
+                LED_CTL_PIN = 1;
+                delay_100us();
+                delay_100us();
+                LED_CTL_PIN = 0;
+                delay_ms(4);
+                LED_CTL_PIN = 1;
+                delay_100us();
+                delay_100us();
+                LED_CTL_PIN = 0;
+                delay_ms(5);
+                LED_CTL_PIN = 1;
+                delay_100us();
+                delay_100us();
+                LED_CTL_PIN = 0;
+                delay_ms(51);
+                send_cmd(0x96);
+                send_cmd(0x45);
+
+                send_cmd(0x96);
+                send_cmd(0x45);
+
+                send_cmd(0x08);
+                send_cmd(0xE0);
+            }
+            else if (ir_data == 0x90) // R
+            {
+                send_cmd(0x0C);
+                send_cmd(0x00);
+                send_cmd(0x00);
+                send_cmd(0x11);
+            }
+            else if (ir_data == 0xB8) // G
+            {
+                send_cmd(0x0C);
+                send_cmd(0x00);
+                send_cmd(0x00);
+                send_cmd(0x22);
+            }
+            else if (ir_data == 0xF8) // B
+            {
+                send_cmd(0x0C);
+                send_cmd(0x00);
+                send_cmd(0x00);
+                send_cmd(0x44);
+            }
+
+            ir_data = 0;
+            flag_is_recved_data = 0;
+        }
+    }
+}
+/************************************************
+;  *    @函数名            : interrupt
+;  *    @说明              : 中断函数
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void int_isr(void) __interrupt
+{
+    __asm;
+    movra _abuf;
+    swapar _PFLAG;
+    movra _statusbuf;
+    __endasm;
+    //=======T0========================
+    if (T0IF & T0IE)
+    {
+        P10D = !P10D;
+        T0IF = 0;
+    }
+    //=======T1========================
+    if (T1IF & T1IE)
+    {
+        P11D = !P11D;
+        T1IF = 0;
+    }
+    //=======T2========================
+    if (T2IF & T2IE)
+    {
+        T2IF = 0;
+
+        static volatile u8 ir_fliter;
+        static volatile u16 ir_level_cnt;      // 红外信号的下降沿时间间隔计数
+        static volatile u8 __ir_data;          //
+        static volatile u16 ir_long_press_cnt; // 檢測紅外遙控長按的計數值
+
+        // 对每个下降沿进行计数
+        ir_level_cnt++;
+
+        ir_fliter <<= 1;
+        if (IR_RECV_PIN)
+        {
+            ir_fliter |= 1;
+        }
+        ir_fliter &= 0x07;
+
+        if (ir_fliter == 0x07)
+            filter_level = 1;
+        else if (ir_fliter == 0x00)
+            filter_level = 0;
+
+        if (filter_level)
+        {
+            last_level_in_ir_pin = 1; // 表示接收到的是高电平
+
+            // 如果之前也是高电平
+            if (ir_level_cnt > 1200) // 超时时间(120000us,120ms)
+            {
+                // if (__ir_data != 0) // 超时，且接收到数据(红外接收处理函数中会把ir_data清零)
+                if (__ir_data != 0) // 超时，且接收到数据(现在是在中断服务函数中把__ir_data自动清零)
+                {
+                    // // 带校验的版本：
+                    // if ((u8)(__ir_data >> 8) == (u8)(~__ir_data)) // 如果键值的原码和反码相等
+                    // {
+                    // flag_is_recved_data = 1;
+                    // }
+
+                    // 不带校验的版本
+                    if (0 == flag_is_recved_data)
+                    {
+                        ir_data = ~__ir_data;
+                        __ir_data = 0;
+                        flag_is_recved_data = 1;
+                    }
+                }
+
+                flag_is_recv_ir_repeat_code = 0; // 认为遥控器按键已经按下，然后松开
+            }
+        }
+        else
+        {
+            if (last_level_in_ir_pin)
+            {
+                // 如果之前检测到的是高电平，现在检测到了低电平
+                if (ir_level_cnt <= 8) // 小于800us，说明接收到无效的数据，重新接收
+                {
+                    // FLAG_IS_RECVED_ERR_IR_DATA = 1;
+                    // flag_is_recv_ir_repeat_code = 0;
+                }
+                else if (ir_level_cnt <= 18) // 小于1800us,说明接收到了逻辑0
+                {
+                    __ir_data <<= 1;
+
+                    // P15D = 0; // 测试红外解码
+                    // P15D = ~P15D; // 测试红外解码
+                }
+                else if (ir_level_cnt <= 26) // 小于2600us,说明接收到了逻辑1
+                {
+                    __ir_data <<= 1;
+                    __ir_data |= 0x01;
+
+                    // P15D = 1; // 测试红外解码
+                }
+                else if (ir_level_cnt <= 150) // 小于15000us,说明接收到了格式头
+                {
+                    // FLAG_IS_RECVED_ERR_IR_DATA = 1;
+                    // ir_long_press_cnt = 0; // 加上这一条会检测不到长按
+                }
+                else if (ir_level_cnt <= 420) // 小于42ms,说明接收完一次完整的红外信号
+                {
+#if 0 // 带校验的版本，命令源码与命令反码进行校验
+
+            if ((u8)(__ir_data >> 8) == (u8)(~__ir_data)) // 如果键值的原码和反码相等
+            {
+                flag_is_recved_data = 1;
+                flag_is_recv_ir_repeat_code = 1; //
+                ir_long_press_cnt = 0;
+            }
+#else // 不带校验的版本
+                    if (0 == flag_is_recved_data)
+                    {
+                        ir_data = ~__ir_data;
+                        __ir_data = 0;
+                        flag_is_recved_data = 1;
+                        flag_is_recv_ir_repeat_code = 1; //
+                        ir_long_press_cnt = 0;
+                    }
+#endif // 不带校验的版本
+                }
+                else if (ir_level_cnt <= 1200) // 小于120000,120ms,说明接收到了重复码
+                {
+                    ir_long_press_cnt++;
+                }
+                else // 超过120000,说明接收到无效的数据
+                {
+                }
+
+                ir_level_cnt = 0;
+            }
+
+            last_level_in_ir_pin = 0; // 表示接收到的是低电平
+        }
+    }
+    //=======T3========================
+    if (T3IF & T3IE)
+    {
+        T3IF = 0;
+    }
+    __asm;
+    swapar _statusbuf;
+    movra _PFLAG;
+    swapr _abuf;
+    swapar _abuf;
+    __endasm;
+}
+
+/**************************** end of file *********************************************/
---
+++ user.h
@@ -0,0 +1,196 @@
+/******************************************************************************
+;  *       @型号                   : MC32F7361
+;  *       @创建日期               : 2021.12.21
+;  *       @公司/作者              : SINOMCU-FAE
+;  *       @晟矽微技术支持         : 2048615934
+;  *       @晟矽微官网             : http://www.sinomcu.com/
+;  *       @版权                   : 2021 SINOMCU公司版权所有.
+;  *---------------------- 建议 ---------------------------------
+;  *                   变量定义时使用全局变量
+******************************************************************************/
+#ifndef USER
+#define USER
+#include "mc32-common.h"
+#include "MC32F7361.h"
+
+/*****************************************************************
+;       Function : Define variables
+;*****************************************************************/
+
+#define u8 unsigned char
+#define u16 unsigned int
+#define u32 unsigned long int
+#define uint8_t unsigned char
+#define uint16_t unsigned int
+#define uint32_t unsigned long int
+
+#define DEF_SET_BIT0 0x01
+#define DEF_SET_BIT1 0x02
+#define DEF_SET_BIT2 0x04
+#define DEF_SET_BIT3 0x08
+#define DEF_SET_BIT4 0x10
+#define DEF_SET_BIT5 0x20
+#define DEF_SET_BIT6 0x40
+#define DEF_SET_BIT7 0x80
+
+#define DEF_CLR_BIT0 0xFE
+#define DEF_CLR_BIT1 0xFD
+#define DEF_CLR_BIT2 0xFB
+#define DEF_CLR_BIT3 0xF7
+#define DEF_CLR_BIT4 0xEF
+#define DEF_CLR_BIT5 0xDF
+#define DEF_CLR_BIT6 0xBF
+#define DEF_CLR_BIT7 0x7F
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
+
+#define USE_MY_DEBUG 0
+
+// const u8 table[] = {
+//     0x01, // ON
+//     0x04,
+//     0x06,
+//     0x07,
+//     0x09,
+//     0x0A,
+
+//     0x0C,
+//     0x0D,
+//     0x0F,
+//     0x10,
+//     0x12,
+//     0x03, // OFF
+// };
+
+volatile u8 cur_led_status; // 记录当前LED的状态
+
+//===============Field Protection Variables===============
+u8 abuf;
+u8 statusbuf;
+
+//===============Global Function===============
+void Sys_Init(void);
+void CLR_RAM(void);
+void IO_Init(void);
+void TIMER0_INT_Init(void);
+void TIMER1_INT_Init(void);
+void TIMER2_INT_Init(void);
+void TIMER3_INT_Init(void);
+
+//============Define  Flag=================
+typedef union
+{
+    unsigned char byte;
+    struct
+    {
+        u8 bit0 : 1;
+        u8 bit1 : 1;
+        u8 bit2 : 1;
+        u8 bit3 : 1;
+        u8 bit4 : 1;
+        u8 bit5 : 1;
+        u8 bit6 : 1;
+        u8 bit7 : 1;
+    } bits;
+} bit_flag;
+volatile bit_flag flag1;
+#define flag_is_recved_data flag1.bits.bit0
+#define flag_is_dev_open flag1.bits.bit1 // 标志位，灯串是否使能
+
+
+#define last_level_in_ir_pin flag1.bits.bit2 // 在红外接收对应的中断函数中，表示上次引脚对应的电平
+#define filter_level flag1.bits.bit3 // 在红外接收对应的中断函数中，表示滤波后的红外信号接收引脚的电平
+#define flag_is_recv_ir_repeat_code flag1.bits.bit4 // 表示是否接收到了红外信号的重复码，用于区分遥控器是否按下按键后松开
+
+#if USE_MY_DEBUG
+
+#define LED_CTL_PIN P22D // 控制灯串的、给灯串发送控制命令的引脚
+
+#else
+
+#define LED_CTL_PIN P04D // 控制灯串的、给灯串发送控制命令的引脚
+
+#endif
+
+// ===================================================
+// 遥控器解码相关配置                                //
+// ===================================================
+#if USE_MY_DEBUG
+#define IR_RECV_PIN P21D // 红外信号接收引脚
+#else
+#define IR_RECV_PIN P15D // 红外信号接收引脚
+#endif
+
+volatile u8 ir_data; // 存放红外接收到的数据
+
+// 毫秒级延时 (误差：在1%以内，1ms、10ms、100ms延时的误差均小于1%)
+// 前提条件：FCPU = FHOSC / 4
+void delay_ms(u16 xms)
+{
+    while (xms)
+    {
+        u16 i = 517;
+        while (i--)
+        {
+            Nop();
+        }
+        xms--; // 把 --操作放在while()判断条件外面，更节省空间
+
+        __asm;
+        clrwdt; // 喂狗
+        __endasm;
+    }
+}
+
+// #if USE_MY_DEBUG
+#define DEBUG_PIN P22D
+#if 1 // 以下程序约占用81字节空间
+// 通过一个引脚输出数据(发送一次约400ms)
+// #define DEBUG_PIN P22D
+void send_data_msb(u32 send_data)
+{
+    // 先发送格式头
+    // __set_input_pull_up(); // 高电平
+    DEBUG_PIN = 1;
+    delay_ms(15);
+    // __set_output_open_drain(); // 低电平
+    DEBUG_PIN = 0;
+    delay_ms(7); //
+
+    for (u8 i = 0; i < 32; i++)
+    {
+        if ((send_data >> (32 - 1 - i)) & 0x01)
+        {
+            // 如果要发送逻辑1
+            // __set_input_pull_up();  	   // 高电平
+            DEBUG_PIN = 1;
+            delay_ms(5); //
+            // __set_output_open_drain(); // 低电平
+            DEBUG_PIN = 0;
+            delay_ms(10); //
+        }
+        else
+        {
+            // 如果要发送逻辑0
+            // __set_input_pull_up();  	   // 高电平
+            DEBUG_PIN = 1;
+            delay_ms(5); //
+            // __set_output_open_drain(); // 低电平
+            DEBUG_PIN = 0;
+            delay_ms(5); //
+        }
+    }
+
+    // 最后，设置为低电平
+    // __set_output_open_drain(); // 低电平
+    DEBUG_PIN = 0;
+    delay_ms(1);
+    DEBUG_PIN = 1;
+    delay_ms(1);
+    DEBUG_PIN = 0;
+}
+#endif // #if USE_MY_DEBUG
+
+#endif
+
+/**************************** end of file *********************************************/
